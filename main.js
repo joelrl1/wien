2372f73162e010bbbe79852677341d549059172e
Joel <joel1.robert@gmail.com>
2022-04-28 17:32:18 +0200

    layer coontrol

--- main.js
+++ main.js
@@ -4,6 +4,21 @@
 let stephansdom={
 lat: 48.208493,
 lng: 16.373118,
-title: "Stephansdom"
+title: "Stephansdom",
 
 };
+
+let startlayer =L.tileLayer.provider("BasemapAT_grau").addTo(map);
+
+let map = L.map("map",{
+center:[ stephansdom.lat, stephansdom.lng], 
+zoom:12,
+layer:[
+    startlayer
+]
+});
+
+let layerControl= L.control.layer({
+    "BasemapAT Grau": startlayer,
+
+}).addTo(map);
