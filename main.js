839b25a144b7a4cfd6420b9d3153168726b7a6a4
Joel <joel1.robert@gmail.com>
2022-05-05 16:40:30 +0200

    Hotels hinzu

--- main.js
+++ main.js
@@ -120,3 +120,19 @@ async function loadZones(url){
     L.geoJSON(geojson).addTo(overlay);
    }
    loadZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json");
+
+     //Hotels und Unterkünfte
+async function loadHotel(url){
+
+    let response = await fetch (url);
+    let geojson = await response.json();
+   // console.log(geojson);
+   
+    let overlay = L.featureGroup();
+   
+   layerControl.addOverlay(overlay, "Hotels und Unterkünfte");
+   overlay.addTo(map);
+   
+    L.geoJSON(geojson).addTo(overlay);
+   }
+   loadHotel("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:UNTERKUNFTOGD&srsName=EPSG:4326&outputFormat=json");
