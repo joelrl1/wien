1eb811047ff728e1a9c91ebb94e0802a62e4e851
Joel <joel1.robert@gmail.com>
2022-05-05 16:35:31 +0200

    Fussgängerzonen hinzugefügt

--- main.js
+++ main.js
@@ -88,3 +88,35 @@ async function loadStops(url){
     L.geoJSON(geojson).addTo(overlay);
    }
    loadStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSVSLOGD&srsName=EPSG:4326&outputFormat=json");
+
+   //Liniennetz Vienna Sightseeing
+async function loadLines(url){
+
+    let response = await fetch (url);
+    let geojson = await response.json();
+   // console.log(geojson);
+   
+    let overlay = L.featureGroup();
+   
+   layerControl.addOverlay(overlay, "Liniennetz Vienna Sightseeing");
+   overlay.addTo(map);
+   
+    L.geoJSON(geojson).addTo(overlay);
+   }
+   loadLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json");
+
+    //Fußgängerzonen Wien
+async function loadZones(url){
+
+    let response = await fetch (url);
+    let geojson = await response.json();
+   // console.log(geojson);
+   
+    let overlay = L.featureGroup();
+   
+   layerControl.addOverlay(overlay, "Fußgängerzonen Wien");
+   overlay.addTo(map);
+   
+    L.geoJSON(geojson).addTo(overlay);
+   }
+   loadZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json");
